d8037e2f7be5e36f58c6ef4d95d0269b6e3f75cf
leon0113 <tahjib.leon399@gmail.com>
2022-10-06 01:26:16 +0600

    primitive data types

--- data-types.js
+++ data-types.js
@@ -1,6 +1,8 @@
 let a = 15;
 let b = 'leon';
 let c = true;
+let d = [12, 23, 20];
 console.log(typeof a);
 console.log(typeof b);
 console.log(typeof c);
+console.log(typeof d);
---
+++ primitive.js
@@ -0,0 +1,22 @@
+// data types
+// primitive data types
+// 1.Number
+// 2.string
+// 3.boolean
+// 4.undefined
+// 5.null
+// 6.symbol
+// non-primitive data types
+// 1.object
+
+let a = 'hello';
+let b = a;
+console.log(b, a);
+a = 'hi';
+console.log(b, a);
+
+const x = { job: 'web developer' };
+const y = x;
+// console.log(x, y);
+x.job = 'front end developer';
+// console.log(x, y)
