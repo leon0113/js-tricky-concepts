1356330a43ab6aeeaab41f940e093684cbf97aef
leon0113 <tahjib.leon399@gmail.com>
2022-10-07 00:53:09 +0600

    undefined vs null

---
+++ undefined-null.js
@@ -0,0 +1,12 @@
+/* cases for undefined
+1. variable value not assigned
+2. function but didn't write return
+3. just wrote return but didn't return anything
+4. parameter that isn't passed
+5. property that doesn't exist in an object
+6. accessing array element out of range
+7. accessing deleted array element
+8. explicitly set value to undefined
+*/
+
+//null means the value isn't there right now
